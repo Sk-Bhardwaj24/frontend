8b295da0e009cfa3b108c62dcd276c3af7e3f53a
saurabh-idc <saurabh.bhardwaj@idreamcareer.com>
2024-05-24 15:29:36 +0530

    removed broken package

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -10,42 +10,11 @@ import {
 } from "../../store/Message";
 import { styled, useTheme } from "@mui/material/styles";
 import ClearIcon from "@mui/icons-material/Clear";
-// const GlobalMessage: React.FC = () => {
-//   const showMessage = useSelector(selectFuseMessageState);
-//   const messageOptions = useSelector(selectFuseMessageOptions);
-//   const dispatch = useDispatch();
-
-//   const handleClose = () => {
-//     dispatch(hideMessage());
-//   };
-
-//   return (
-//     <Snackbar
-//       open={showMessage}
-//       anchorOrigin={messageOptions.anchorOrigin}
-//       autoHideDuration={messageOptions.autoHideDuration}
-//       onClose={handleClose}
-//       message={messageOptions.message}
-//       // Additional props can be passed here, e.g., variant={messageOptions.variant}
-//     />
-//   );
-// };
-
-// export default GlobalMessage;
 import { amber, blue, green } from "@mui/material/colors";
-// import { styled } from '@mui/material/styles';
 import IconButton from "@mui/material/IconButton";
-// import Snackbar from '@mui/material/Snackbar';
 import SnackbarContent from "@mui/material/SnackbarContent";
 import Typography from "@mui/material/Typography";
 import { memo } from "react";
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   hideMessage,
-//   selectFuseMessageOptions,
-//   selectFuseMessageState,
-// } from 'app/store/fuse/messageSlice';
-// import FuseSvgIcon from '../FuseSvgIcon';
 interface FuseMessageProps {
   variant: "success" | "warning" | "error" | "info";
   theme: any;
